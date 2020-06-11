623c534de5358e26e64493052c8ed85f0b99b208
zdamao <zdamao@126.com>
2020-06-11 23:03:59 +0800

    finish accordin

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,11 @@
     const origOffsetY = $navbar.offset().top;
     function scroll() {
       if ($(window).scrollTop() >= origOffsetY) {
-        $navbar.addClass("fixed-top");
+        $navbar.addClass("fixed-top").css("top", 0);
       } else {
-        $navbar.removeClass("fixed-top");
+        $(window).width() >= 1140
+          ? $navbar.css("top", "38px")
+          : $navbar.css("top", "70px");
       }
     }
     document.onscroll = scroll;
@@ -26,4 +28,25 @@
     //   // $(".btn:first-child").val($(this).text());
     // });
   });
+
+  $("#accordionQA").on(
+    "hide.bs.collapse show.bs.collapse",
+    ".panel-collapse",
+    function (e) {
+      let $this = $(this);
+      $this.prev().find(".ico-expandable").toggleClass("ico-expanded");
+    }
+  );
+  // Add minus icon for collapse element which is open by default
+  // $(".collapse.in").each(function () {
+  //   $(this).siblings(".panel-heading").find(".glyphicon").addClass("rotate");
+  // });
+  // // Toggle plus minus icon on show hide of collapse element
+  // $(".collapse")
+  //   .on("show.bs.collapse", function () {
+  //     $(this).parent().find(".glyphicon").addClass("rotate");
+  //   })
+  //   .on("hide.bs.collapse", function () {
+  //     $(this).parent().find(".glyphicon").removeClass("rotate");
+  //   });
 })(jQuery);
